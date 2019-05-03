b0c003a9009fdf1ba598ef815c93a98b1468bf43
Joe Pritchett <joempritchett@gmail.com>
2019-05-03 11:03:07 -0700

    add new react app for practice, update readme

---
+++ expoTesting/practiceProject/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginTop:'50%',
+  },
+});
--- readme.md
+++ readme.md
@@ -6,3 +6,5 @@
 |8:00am|Begin reading React Native documentation.|
 |8:30am|Following the tutorial in the React Native documentation, create demo project using Expo CLI and view project on phone via Expo App.|
 |9:00am|Begin reading Expo documentation.|
+|10:00am|Continue reading Expo documentation.|
+|10:45am|Begin work on practice React app, following tutorial from React Native documentation|
